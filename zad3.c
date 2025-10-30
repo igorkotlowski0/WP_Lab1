cc810ca4dfcb7e540e3b5a758c0b87bac5c3f507
igorkotlowski0 <igorkotlowski5@gmail.com>
2025-10-30 18:46:31 +0000

    test

--- zad3.c
+++ zad3.c
@@ -3,16 +3,28 @@
 int main() {
     float temperatura;
     char kierunek;
+    
    
+
     printf("podaj temperature");
     scanf("%f", &temperatura);
-    printf("wybierz C lub F")
-    scanf("%c" &kierunek)
+    printf("wybierz z czego zamieniasz: C lub F");
+    scanf("%c" &kierunek);
 
-    if ("%c" = 'C') {
-        printf();
+
+    if (kierunek== 'C'){
+        temperatura = temperatura *1.8 + 32;
+        printf("wynik %d", temperatura);
     }
-    else ("%c" = 'F')
+    else {
+        printf("&temperatura-32 / 1.8 st C");
+    }
+    
+    if (kierunek == 'F'); {
+        printf("&temperatura-32 / 1.8 st C");
+        }
+        
+    }
 
     return 0;
 }
